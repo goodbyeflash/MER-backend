d7a6585e509eb03a6f33c984879887007cf4edef
goodbyeFlash <goodbyeflash.dev@gmail.com>
2022-08-02 00:20:12 +0900

    CORS 적용

--- src/main.js
+++ src/main.js
@@ -28,6 +28,13 @@ const router = new Router();
 // 라우터 설정
 router.use('/api', api.routes()); // api 라우트 적용
 
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  ctx.set('Access-Control-Allow-Methods', 'POST, GET, DELETE, PATCH');
+  await next();
+});
+
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
 app.use(jwtMiddleware);
@@ -35,15 +42,15 @@ app.use(jwtMiddleware);
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-const buildDirectory = path.resolve(__dirname, '../../frontend/build');
-app.use(serve(buildDirectory));
-app.use(async (ctx) => {
-  // Not Found이고, 주소가 /api로 시작하지 않는 경우
-  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
-    // index.html 내용을 반환
-    await send(ctx, 'index.html', { root: buildDirectory });
-  }
-});
+// const buildDirectory = path.resolve(__dirname, '../../frontend/build');
+// app.use(serve(buildDirectory));
+// app.use(async (ctx) => {
+//   // Not Found이고, 주소가 /api로 시작하지 않는 경우
+//   if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+//     // index.html 내용을 반환
+//     await send(ctx, 'index.html', { root: buildDirectory });
+//   }
+// });
 
 // PORT가 지정되어 있지 않다면 4000을 사용
 const port = PORT || 4000;
